a5dfdcfcfd4a7d2e8b10f7235aa96bb8a17a82b4
Mike <michael.ries@gmail.com>
2017-11-04 14:03:53 -0500

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,20 +1,29 @@
 function ContactController() {
-    this.contacts = [{
-        name: 'Bob',
-        phone: '0123458690'
-    },{
-        name: 'Tim',
-        phone: '3934203242'
-    },{
-        name: 'Ross',
-        phone: '0684059433'
-    }];
+    this.contacts = [
+        {
+            name: "Bob",
+            phone: "0123458690"
+        },
+        {
+            name: "Tim",
+            phone: "3934203242"
+        },
+        {
+            name: "Ross",
+            phone: "0684059433"
+        }
+    ];
 
-    this.removeContact = function (index) {
+    this.removeContact = function(index) {
         this.contacts.splice(index, 1);
-    }
+    };
+
+    this.submitForm = function() {
+        this.contacts.push({
+            name: this.name,
+            phone: this.phoneNumber
+        });
+    };
 }
 
-angular
-    .module('app')
-    .controller('ContactController', ContactController);
+angular.module("app").controller("ContactController", ContactController);
